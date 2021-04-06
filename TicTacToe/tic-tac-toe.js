f39fd4589f036178b47e64fad7cdbac7d4b311fa
My Name <66422260+Radek25@users.noreply.github.com>
2021-04-06 18:59:24 +0200

    Add signs (O and X) functionality

--- TicTacToe/tic-tac-toe.js
+++ TicTacToe/tic-tac-toe.js
@@ -4,10 +4,31 @@ var BlockArray = [
     [], [], [],
     [], [], []
 ];
-for (var i = 0; i < BlockArray.length; i++) {
+var _loop_1 = function (i) {
     var Block = document.createElement('div');
     Block.setAttribute('id', 'block' + i.toString());
+    Block.addEventListener('click', function () { return AddSign(i, Block); });
     Block.classList.add('block');
     GameBoard.appendChild(Block);
+};
+//Creating a signs boards
+for (var i = 0; i < BlockArray.length; i++) {
+    _loop_1(i);
 }
-console.log(BlockArray);
+var TypeOfSignSwitch = true;
+//Adding signs to boards
+function AddSign(IndexOfBlock, Block) {
+    if (TypeOfSignSwitch == true) {
+        TypeOfSignSwitch = !TypeOfSignSwitch;
+        Block.innerHTML = '<i class="far fa-circle"></i>';
+        BlockArray[IndexOfBlock] = ['O'];
+        console.log(IndexOfBlock);
+    }
+    else {
+        TypeOfSignSwitch = !TypeOfSignSwitch;
+        Block.innerHTML = '<i class="fas fa-times"></i>';
+        BlockArray[IndexOfBlock] = ['X'];
+        console.log(IndexOfBlock);
+    }
+    console.log(BlockArray);
+}
--- TicTacToe/tic-tac-toe.ts
+++ TicTacToe/tic-tac-toe.ts
@@ -1,16 +1,39 @@
+enum Sings{
+    X = 'X',
+    O = 'O'
+}
+
 let GameBoard : Element = document.querySelector('#game-board');
 
-let BlockArray = [
+let BlockArray: string[][] = [
     [], [], [],
     [], [], [],
     [], [], []
 ];
 
-
+//Creating a signs boards
 for (let i = 0; i < BlockArray.length; i++) {
        let Block = document.createElement('div');
        Block.setAttribute('id', 'block' + i.toString());
+       Block.addEventListener('click', ()=>AddSign(i, Block));
        Block.classList.add('block');
        GameBoard.appendChild(Block);
 }
-console.log(BlockArray);
+
+let TypeOfSignSwitch : boolean = true;
+//Adding signs to boards
+function AddSign(IndexOfBlock : number, Block : HTMLDivElement){
+    if (TypeOfSignSwitch == true) {
+        TypeOfSignSwitch = !TypeOfSignSwitch;
+        Block.innerHTML = '<i class="far fa-circle"></i>';
+        BlockArray[IndexOfBlock] = [Sings.O];
+        console.log(IndexOfBlock);
+    }
+    else{
+        TypeOfSignSwitch = !TypeOfSignSwitch;
+        Block.innerHTML = '<i class="fas fa-times"></i>';
+        BlockArray[IndexOfBlock] = [Sings.X];
+        console.log(IndexOfBlock);
+    }
+    console.log(BlockArray);
+}
