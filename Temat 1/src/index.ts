45d80b11b9832f9b916b76bc054e385a235e2955
My Name <66422260+Radek25@users.noreply.github.com>
2021-06-09 00:04:50 +0200

    Topic1 - first and second task

--- Temat 1/src/index.ts
+++ Temat 1/src/index.ts
@@ -12,7 +12,7 @@ const div2 = document.createElement('div');
 div2.classList.add('div');
 document.body.appendChild(div2);
 
-class Person {
+class PersonTaskOne {
     Name: string;
     Surname: string;
     Age: number;
@@ -28,5 +28,58 @@ class Person {
     }
 }
 
-let p1 = new Person('John', 'Black', 11);
+let p1 = new PersonTaskOne('John', 'Black', 11);
 p1.Show();
+
+//Zadanie 2
+
+interface Person {
+    // TODO: dodać pola: imię (string), nazwisko (string), wiek (number) oraz rola (string)
+    name: string;
+    surname: string;
+    age: number;
+    role: string;
+    }
+    
+    const users: Person[] = [
+        { name: 'John', surname: 'Smith', age: 25, role: 'user'},
+        { name: 'Adam', surname: 'Johnson', age: 35, role: 'user'},
+        { name: 'Andy', surname: 'Cole', age: 18, role: 'user'},
+    ]
+    
+    const admins: Person[] = [
+        { name: 'Matthew', surname: 'Ryan', age: 43, role: 'admin'},
+        { name: 'Adam', surname: 'Terry', age: 24, role: 'admin'},
+    ]
+    
+    function logPerson(person: Person) {
+    // TODO: dodać wypisywanie na konsoli danych osoby: "imię nazwisko, wiek, rola"
+        console.log(`${person.name} ${person.surname} ${person.age} ${person.role}`)
+    }
+    
+    function filterPersons(persons: Person[], criteria: any): Person[] {
+    // TODO: zaimplementować funkcję, która przefiltruje tablicę persons za pomocą predykatu criteria
+        return persons.filter(person => person.name == criteria.name || person.surname == criteria.surname || person.age == criteria.age || person.role == criteria.role);
+    }
+    
+    // TODO:
+    // 1. Przy pomocy funkcji logPerson wypisać osoby z tablicy users i admins (patrz foreach)
+    console.log('***Zadanie1***')
+    users.forEach(logPerson);
+    admins.forEach(logPerson);
+    console.log('--------------------------')
+    // 2. Złączyć tablice users i admins i wypisać zawartość złączonej tablicy na konsoli (patrz operator spread)
+    console.log('***Zadanie2***')
+    let usersAndAdmins: Person[] = [...users, ...admins];
+    usersAndAdmins.forEach(usersAndAdmins => logPerson(usersAndAdmins));
+    console.log('--------------------------')
+    //3. Wypisać osoby powyżej 25 lat (patrz operator filter)
+    console.log('***Zadanie3***')
+    const filteredAge = usersAndAdmins.filter(usersAndAdmins => usersAndAdmins.age > 25);
+    filteredAge.forEach(logPerson);
+    console.log('--------------------------')
+    // 4. Wypisać osoby o imieniu Adam (zaimplementować funkcję filterPersons) -> const filtered = filterPersons(persons, { name: 'Adam' });
+    console.log('***Zadanie4***');
+    const filteredName = filterPersons(usersAndAdmins, {name: 'Adam'});
+    filteredName.forEach(logPerson);
+    console.log('--------------------------')
